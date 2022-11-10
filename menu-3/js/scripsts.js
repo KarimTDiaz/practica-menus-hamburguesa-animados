342ad033ab2f98ee879153548729c7fc6e31cc67
karimtd <karimtilouni94@gmail.com>
2022-11-10 22:12:17 +0100

    Práctica menús hamburguesa animados

---
+++ menu-3/js/scripsts.js
@@ -0,0 +1,6 @@
+const menu=document.getElementById('menu')
+const menuIcon=document.getElementById('menu-icon')
+
+menu.addEventListener('click', () =>{
+    menuIcon.classList.toggle('menu-icon__bar--click')
+}) 
